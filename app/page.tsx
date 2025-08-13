bd956e350d5d123d541c6f90ddb8ee10560da4ef
yannsunn <yannsunn1116@gmail.com>
2025-08-13 16:43:55 +0900

    🗑️ 架空データを含むセクションを削除

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import HeroSection from '@/components/sections/HeroSection'
 import AboutSection from '@/components/sections/AboutSection'
-import ServicesSection from '@/components/sections/ServicesSection'
 import WorksSection from '@/components/sections/WorksSectionNew'
-import TestimonialsSection from '@/components/sections/TestimonialsSection'
 import PricingSection from '@/components/sections/PricingSection'
 import ProcessSection from '@/components/sections/ProcessSection'
 import ContactSection from '@/components/sections/ContactSection'
@@ -12,9 +10,7 @@ export default function HomePage() {
     <>
       <HeroSection />
       <AboutSection />
-      <ServicesSection />
       <WorksSection />
-      <TestimonialsSection />
       <PricingSection />
       <ProcessSection />
       <ContactSection />
